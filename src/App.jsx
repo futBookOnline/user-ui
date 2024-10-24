d43fc54f35012d53e48b3e19e002db0f8564c2a2
unknown <sachinkhatri53@gmail.com>
2024-10-24 23:13:55 +0545

    changed url for socket io

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import PublicRouter from "./routes/PublicRouter"
 import io  from "socket.io-client"
-export const socket = io('http://localhost:3000');
+// export const socket = io('http://localhost:3000');
+export const socket = io('https://user-api-k6g6.onrender.com');
 
 function App() {
   return (
